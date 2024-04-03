b4f30d9f3a2b2d22a745e722640282f10ee0e2e9
bjrmahmoud <mahmoud.bjr005@gmail.com>
2024-04-03 12:27:00 +0000

    First commit

---
+++ src/main/java/ma/emsi/hopital/HopitalApplication.java
@@ -0,0 +1,26 @@
+package ma.emsi.hopital;
+
+import ma.emsi.hopital.entities.Patient;
+import ma.emsi.hopital.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class HopitalApplication implements CommandLineRunner {
+    @Autowired
+    private PatientRepository patientRepository;
+    public static void main(String[] args) {
+        SpringApplication.run(HopitalApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        patientRepository.save(new Patient(null,"Mohamed",new Date(),false,34));
+        patientRepository.save(new Patient(null,"Hanane",new Date(),false,4321));
+        patientRepository.save(new Patient(null,"Imane",new Date(),false,34));
+    }
+}
